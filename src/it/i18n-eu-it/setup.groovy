99165ca183f4148f7ac742f6b2320e26d96946e8
(w) Iker Hernaez <ihernaez@users.noreply.github.com>
2025-06-14 12:55:34 +0200

    Updated license headers

--- src/it/i18n-eu-it/setup.groovy
+++ src/it/i18n-eu-it/setup.groovy
@@ -2,7 +2,7 @@
  * #%L
  * basque-locale-extension
  * %%
- * Copyright (C) 2024 Orne Developments
+ * Copyright (C) 2024 - 2025 Orne Developments
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
--- src/test/java/dev/orne/i18n/spi/eu/BasqueLocaleServiceProviderTest.java
+++ src/test/java/dev/orne/i18n/spi/eu/BasqueLocaleServiceProviderTest.java
@@ -4,7 +4,7 @@ package dev.orne.i18n.spi.eu;
  * #%L
  * basque-locale-extension
  * %%
- * Copyright (C) 2024 Orne Developments
+ * Copyright (C) 2024 - 2025 Orne Developments
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
